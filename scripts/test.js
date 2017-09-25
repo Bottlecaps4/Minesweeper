65953cbbf1e9e6cfdbb28041ca1e23dc6d82b707
Brian O Halloran <brianoh8@gmail.com>
2017-09-25 14:36:49 +0100

    removed canvas in place of grid of divs

--- scripts/test.js
+++ scripts/test.js
@@ -1,73 +1,38 @@
 $(document).ready(function(){
     // Global variables
 
-    const canvas = $('#canvas')[0],
-          ctx = canvas.getContext("2d"),
-          canvasW = $("#canvas").width(),
-          canvasH = $("#canvas").height();
-    
-    var grid;
-    var cols; 
-    var rows; 
-    var w = 40;
-    
-    // Cell object
-    function Cell(x, y, w){
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        if (Math.random(1) < 0.5){
-            this.bee = true;
-        } else {
-            this.bee = false;
-        }
-        
-        this.revealed = true;
-    }
-    
-    // Show function to display cell on canvas
-    Cell.prototype.show = function(){
-            ctx.strokeStyle = "rgb(0,0,0";
-            ctx.strokeRect(this.x, this.y, this.w, this.w);
-            if(this.revealed){
-                if(this.bee){
-                    ctx.beginPath();
-                    ctx.arc(this.x + this.w/2 , this.y + this.w/2, this.w/4, 0,  2*Math.PI)
-                    ctx.stroke()
-                    ctx.fillStyle = "rgb(255, 255, 255)"
-                    ctx.fill()
-                    ctx.closePath()
-                }
-            }
-    }      
-    
-    // generic function to make a grid (an array of arrays)
-    function make2DArray(cols, rows){
-        var arr = new Array(cols);
-        for(var i=0; i < arr.length; i ++){
-            arr[i] = new Array(rows)
-        }
-        return arr;
-    }
+    const boardW = $("#container").width(),
+          boardH = $("#container").height();
+          
+    var cols = 9; 
+    var rows = 9;
+    var numBombs=10;
+    var bomb; 
 
-    // Give the canvas a background of white to begin with 
-    function paintCanvas(){
-        ctx.fillStyle = "rgb(255, 255, 255)";
-        ctx.fillRect(0, 0, canvasW, canvasH );
-    }
-    paintCanvas();
-
-    // Create Cell objects on the canvas
-    function makeCell(){
-        cols = Math.floor( canvasW/ w);
-        rows = Math.floor( canvasH / w);
-        grid = make2DArray(cols, rows);
-        for (var i = 0; i < cols; i ++){
-            for(var j = 0; j < rows; j++){
-                grid[i][j] = new Cell(i*w, j*w, w);
-                grid[i][j].show();
+    // Create the grid
+    function makeGrid(cols, rows){
+        for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                $('#container').append('<div class="square '+i+j+'""></div>');   
             }
+        $('.square').width(boardW/cols);
+        $('.square').height(boardH/rows);
         }
     }
-    makeCell();
-})
+    makeGrid(9, 9);
+    
+    // Creates a bomb
+    function makeBomb(){
+        bomb = {
+            x: Math.floor(Math.random()*cols),
+            y: Math.floor(Math.random()*rows),
+        }
+        $("."+bomb.x+bomb.y+"").addClass('bee');
+        // console.log("."+bomb.x+bomb.y+"");
+    }
+    
+    // Make the bomb numBombs amount of times
+    for (var i = 0; i < numBombs; i++) {
+        makeBomb();  
+    }
+})  
